160ce568582a8c502f9edebcd1471369bda81b12
jnfmunoz <juanmunozgrau@gmail.com>
2025-04-28 23:55:23 -0400

    5

--- for_foreach.php
+++ for_foreach.php
@@ -1,14 +1,31 @@
 <?php
 
-    $base = 2;
-    $exponente = 5;
+    // $base = 2;
+    // $exponente = 5;
 
-    $resultado = 1;
+    // $resultado = 1;
 
-    for($i = 0; $i < $exponente; $i++){
-        $resultado *= $base;
+    // for($i = 1; $i <= $exponente; $i++){
+    //     $resultado *= $base;
+    // }
+
+    // echo "El valor de $base elevado a $exponente es: $resultado";
+
+    // $filas = 10;
+
+    // for($i = 1; $i <= $filas; $i++){
+
+    //     for( $j = 1; $j <= $i; $j++){
+    //         echo "*";
+    //     }
+    //     echo "<br>";
+    // }
+
+    $nombres = ['Juan', 'Francisco', 'Camilo', 'Andrés'];
+    // echo $nombres[0];
+
+    foreach ($nombres as $indice => $nombre) {
+        echo "id: ". $indice . " Nombre: " . $nombre . "<br>";
     }
-
-    echo "El valor de $base elevado a $exponente es: $resultado";
 
 ?>
