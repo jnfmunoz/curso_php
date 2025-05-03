c2efc9e558e359ee38859c8f7d86d17616dfd0b7
jnfmunoz <juanmunozgrau@gmail.com>
2025-05-03 19:17:08 -0400

    9

---
+++ ambito_de_las_variables.php
@@ -0,0 +1,22 @@
+<?php
+    $a = 5;
+    $b = 10;
+
+    /* function test(){
+        global $a; 
+        $a = 3;
+        echo $a;
+    }  
+
+    test(); */
+
+    function test(&$n){
+        $n = $n + 10;
+    }
+
+    test($a);
+    test($b);
+    echo "<br>";
+    echo $a . "<br>" . $b;
+    
+?>
