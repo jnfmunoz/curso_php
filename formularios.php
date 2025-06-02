e2e5a3bce5aea67a75706dcd030caf4c597e4d4f
jnfmunoz <juanmunozgrau@gmail.com>
2025-06-02 02:49:42 -0400

    16

--- formularios.php
+++ formularios.php
@@ -6,14 +6,52 @@
     <title>Formularios</title>
 </head>
 <body>
-    <form action="procesar.php" method="GET">
-    <!-- <form action="procesar.php" method="POST"> -->
+    <!-- <form action="procesar.php" method="GET"> -->
+    <form action="procesar.php" method="POST" enctype="multipart/form-data">
         <label>
             Nombre:
             <input type="text" name="name">
         </label>
         <br>
+        <br>
+        <label>
+            Edad:
+            <input type="number" name="edad">
+        </label>
+        <br>
+        <p>Sexo: </p>
+        <label>
+            <input type="radio" name="sexo" value="Masculino">
+            Masculino:
+        </label>
+        <label>
+            <input type="radio" name="sexo" value="Femenino">
+            Femenino:
+        </label>
+        <br>
+        <p>Roles: </p>
+        <label>
+            <input type="checkbox" name="roles[]" value="Administrador">
+            Administrador
+        </label>
+        <label>
+            <input type="checkbox" name="roles[]" value="Editor">
+            Editor
+        </label>
+        <label>
+            <input type="checkbox" name="roles[]" value="Moderador">
+            Moderador
+        </label>
+        <br>
+        <br>
+        <label>
+            Imagen
+            <input type="file" name="image" src="" alt="">
+        </label>
+        <br>
+        <br>
         <button type="submit">Enviar</button>
+        
     </form>
 </body>
 </html>
--- procesar.php
+++ procesar.php
@@ -1,6 +1,21 @@
 <?php
-    $nombre = $_GET['name']; // $_POST['name'];
+    $nombre =  $_REQUEST['name']; //$_GET['name']; // $_POST['name'];
+    $edad = $_REQUEST['edad'];
+    $sexo = $_REQUEST['sexo'];
+    $roles = $_REQUEST['roles'];
+
+    $image = $_FILES['image'];
 
     echo "<p>El nombre del usuario es: $nombre </p>";
+    echo "<p>La edad del usuario es: $edad </p>";
+    echo "<p>El sexo del usuario es: $sexo </p>";
+    echo "<p>El/los rol/es del usuario es/son: </p>";
 
+    echo "<ul>";
+    foreach ($roles as $indice => $rol) {
+        echo "<li>$rol</li>";
+    }
+    echo "</ul>";
+
+    var_dump($image);
 ?>
