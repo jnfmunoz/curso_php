55898690f601b8074b7b1b709837a00db85b9dee
jnfmunoz <juanmunozgrau@gmail.com>
2025-06-30 02:52:14 -0400

    21

--- objetos/clases/Persona.php
+++ objetos/clases/Persona.php
@@ -1,6 +1,20 @@
 <?php
     class Persona{
-        public $nombre, $nombre, $edad;
+        public $nombre, $apellido, $edad;
+
+        public function __construct(){
+            // voy en la clase 23 min 20.56
+            // https://www.youtube.com/watch?v=BSsx7QGlhpo&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=31
+        }
+
+        public function setNombre($nombre){
+            $this->nombre = strtolower($nombre);
+
+        }
+
+        public function getNombre(){
+            return ucwords($this->nombre);
+        }
 
     }
 
--- objetos/index.php
+++ objetos/index.php
@@ -1,4 +1,25 @@
 <?php
 
     require_once('clases/Persona.php');
+
+    $persona1 = new Persona;
+
+    $persona1->setNombre("JuAn FrAncIsco");
+    $persona1->apellido = "Muñoz Grau";
+    $persona1->edad = 26;
+
+    $persona2 = new Persona;
+
+    $persona2->nombre = "Camilo Andrés";
+    $persona2->apellido = "Muñoz Grau";
+    $persona2->edad = 29;
+
+    // var_dump($persona1);
+    // echo "<br>";
+    // var_dump($persona2);
+
+    echo "El nombre de la persona 1 es: " . $persona1 -> getNombre() . " " . $persona1 -> apellido;
+    echo "<br>";
+    echo "El nombre de la persona 2 es: " . $persona2 -> nombre . " " . $persona2 -> apellido;
+
 ?>
