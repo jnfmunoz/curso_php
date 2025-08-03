7ebfd2806869f72af7e48372cea1888f83a15ec2
jnfmunoz <juanmunozgrau@gmail.com>
2025-08-02 23:55:47 -0400

    22

--- objetos/clases/Persona.php
+++ objetos/clases/Persona.php
@@ -2,9 +2,7 @@
     class Persona{
         public $nombre, $apellido, $edad;
 
-        public function __construct(){
-            // voy en la clase 23 min 20.56
-            // https://www.youtube.com/watch?v=BSsx7QGlhpo&list=PLZ2ovOgdI-kUSqWuyoGJMZL6xldXw6hIg&index=31
+        public function __construct(){        
         }
 
         public function setNombre($nombre){
